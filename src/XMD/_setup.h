835bafd55a4ecf71cf2b76acef37084c9cab32e2
Guus Bonnema <gbonnema@xs4all.nl>
2014-12-22 11:48:45 +0100

    Added license related material. Modified licenses of all files.

--- src/XMD/_setup.h
+++ src/XMD/_setup.h
@@ -1,4 +1,3 @@
-
 /************************************************************************
  *
  * Copyright Stefan van der Sluys, 2014
@@ -23,23 +22,18 @@
 #ifndef _SETUP
 #define _SETUP
 
-
-
 #define TOOL_NAME                   "XMAS Model Designer 2015"
 #define TOOL_VERSION                "0.1.0"
-
 
 #define OLD_FILE_EXTENSION          "wck"
 #define JSON_FILE_EXTENSION         "json"
 #define FJSON_FILE_EXTENSION        "fjson"
-
 
 #define MODEL_FILE_EXTENSION        "xmdm"
 #define PROJECT_FILE_EXTENSION      "xmdp"
 #define COMPONENT_FILE_EXTENSION    "xmdc"
 
 #define MAXIMUM_RECENT_FILES        5
-
 
 #endif // _SETUP
 
