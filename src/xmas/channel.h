db72ad9d10b4d650761d8d4d3e175a1dccb932b3
Guus Bonnema <gbonnema@xs4all.nl>
2014-12-27 21:34:25 +0100

    Still working on noc library

--- src/xmas/channel.h
+++ src/xmas/channel.h
@@ -1,3 +1,24 @@
+/*********************************************************************
+ *
+ * Copyright (C) Guus Bonnema, 2014
+ *
+ * This file is part of the xmas-design tool.
+ *
+ * The xmas-design tool is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * The xmas-design tool is distributed in the hope that it will be
+ * useful,  but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the xmas-design tool.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ **********************************************************************/
 #ifndef CHANNEL_H
 #define CHANNEL_H
 
@@ -21,8 +42,8 @@ public:
 private:
 
    QString m_name;
-   std::shared_ptr<InPort> m_in;
    std::shared_ptr<OutPort> m_out;
+   std::shared_ptr<InPort> m_in;
 
 };
 
--- src/xmas/noc.h
+++ src/xmas/noc.h
@@ -1,3 +1,24 @@
+/*********************************************************************
+ *
+ * Copyright (C) Guus Bonnema, 2014
+ *
+ * This file is part of the xmas-design tool.
+ *
+ * The xmas-design tool is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * The xmas-design tool is distributed in the hope that it will be
+ * useful,  but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the xmas-design tool.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ **********************************************************************/
 #ifndef NOC_H
 #define NOC_H
 
@@ -32,13 +53,13 @@ class Noc : public QObject
 public:
 
     explicit Noc(QString name);
-    explicit Noc(QDataStream nocDataStream);
     ~Noc();
 
     const QString name() const;
 
     Noc &add(std::shared_ptr<Component> );
-    Noc &add(std::shared_ptr<Port> port);
+    Noc &add(std::shared_ptr<InPort> inPort);
+    Noc &add(std::shared_ptr<OutPort> outPort);
     Noc &add(std::shared_ptr<Channel> channel);
 
     bool valid();
@@ -48,7 +69,8 @@ public:
 signals:
 
     void componentAdded(std::shared_ptr<const Component> comp);
-    void portAdded(std::shared_ptr<const Port> port);
+    void inPortAdded(std::shared_ptr<const InPort> inport);
+    void outPortAdded(std::shared_ptr<const OutPort> outport);
     void channelAdded(std::shared_ptr<const Channel> channel);
 
 private:
@@ -61,9 +83,13 @@ private:
      */
     QMap<QString, std::shared_ptr<Component>> m_comp_map;
     /**
-     * @brief m_port_map map of ports in Noc by name
+     * @brief m_inport_map map of InPorts in Noc by name
      */
-    QMap<QString, std::shared_ptr<Port>> m_port_map;
+    QMap<QString, std::shared_ptr<InPort>> m_inport_map;
+    /**
+     * @brief m_outport_map map of OutPorts in Noc by name
+     */
+    QMap<QString, std::shared_ptr<OutPort>> m_outport_map;
 
     /**
      * @brief m_connection_map map of connections.
