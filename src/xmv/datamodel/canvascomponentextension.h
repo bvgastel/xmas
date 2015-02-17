94d5f5ced1468fa4324894d8f1c0e523075f9f8a
JK <j.kleijn@studie.ou.nl>
2015-02-17 21:59:32 +0100

    Update CanvasComponentExtension

--- src/xmv/datamodel/canvascomponentextension.h
+++ src/xmv/datamodel/canvascomponentextension.h
@@ -11,6 +11,21 @@ public:
       : m_x{x}, m_y{y}, m_orientation{orientation}, m_scale{scale}
     {}
 
+    // NOTE: Extension currently has a copy constructor and copy assignment operator
+    // Using these functions (e.g. in derived Extension classes) can compromise
+    // the internal "E* next" pointer which will be copied (overwritten) as well.
+    // Suggestion: delete the copy constructor and assignment functions in Extension.
+
+    // In the custom implementations defined below, the base Extension members are not copied.
+    CanvasComponentExtension(CanvasComponentExtension& b)
+      : m_x{b.m_x}, m_y{b.m_y}, m_orientation{b.m_orientation}, m_scale{b.m_scale}
+    {}
+
+    operator =(CanvasComponentExtension& b)
+    {
+        m_x = b.m_x; m_y = b.m_y; m_orientation = b.m_orientation; m_scale = b.m_scale;
+    }
+
     int x() const           { return m_x; }
     int y() const           { return m_y; }
     int orientation() const { return m_orientation; }
@@ -24,4 +39,3 @@ private:
 };
 
 #endif // CANVASCOMPONENTEXTENSION
-
