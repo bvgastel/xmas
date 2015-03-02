755a2ddb34b2b8290b4df85c950371daf459b013
Guus Bonnema <gbonnema@xs4all.nl>
2015-03-02 21:51:05 +0100

    Removed useless logger statements.

--- src/xmd/xmd/xmapper/parseflatjsonfile.cpp
+++ src/xmd/xmd/xmapper/parseflatjsonfile.cpp
@@ -20,22 +20,15 @@ bool ParseFlatJsonFile::parseJsonFile (const QUrl &fileUrl,
                                        std::map<bitpowder::lib::String, XMASComponent *> &componentMap,
                                        bitpowder::lib::MemoryPool &mp) {
 
-    Logger *m_logger = new Logger("parse flat json");
 
     std::string filename =
             fileUrl.isLocalFile() ? fileUrl.toLocalFile().toStdString()
                                   : fileUrl.fileName().toStdString();
 
-    m_logger->log("Opening file " + filename, QColor("black"));
-
-    m_logger->log("Started process validate network for file '"
-                  + filename);
-
-
     std::tie(componentMap, std::ignore) = parse_xmas_from_file(filename, mp);
 
     if (componentMap.empty()) {
-        m_logger->log("[Component.cpp/fileOpen(fileUrl)] File "+ filename + " was parsed as empty. Maybe the file is invalid json input.",Qt::red);
+        std::cout << "Empty file offered for parsing: "+fileUrl.fileName().toStdString() << std::endl;
         return false;
     }
 
