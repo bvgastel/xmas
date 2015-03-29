4a0c765690a6114cbab8981381f6fc3d8bd441e3
JK <j.kleijn@studie.ou.nl>
2015-03-29 12:47:04 +0200

    Made loadNetwork public

--- src/xmv/datamodel/xmasproject.h
+++ src/xmv/datamodel/xmasproject.h
@@ -16,6 +16,7 @@ public:
     XMASNetwork* getRootNetwork() const { return root; }
     const XMASNetwork* getNetwork(const std::string name) const { return networks.find(name)->second.get(); }
 
+    XMASNetwork* loadNetwork(const std::string& filename);
     void saveNetwork(const std::string& filename, XMASNetwork* network = nullptr);
 
     template <class T, typename... Args>
@@ -28,7 +29,6 @@ private:
     std::map<std::string, std::unique_ptr<XMASNetwork>>  networks;
     XMASNetwork* root;
 
-    XMASNetwork* loadNetwork(const std::string& filename);
 };
 
 #endif // XMASPROJECT_H
